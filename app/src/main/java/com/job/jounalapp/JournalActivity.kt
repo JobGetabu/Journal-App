2e1218e825542bfeba4f2bb063cdbf1864382543
JobGetabu <getabujob@gmail.com>
2018-06-25 14:31:03 +0300

    first commit

---
+++ app/src/main/java/com/job/jounalapp/JournalActivity.kt
@@ -0,0 +1,12 @@
+package com.job.jounalapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class JournalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_journal)
+    }
+}
