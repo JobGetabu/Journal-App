1e86df1ca4c7bf7d2ec042bfd5e8a93a1e2a1ff5
JobGetabu <getabujob@gmail.com>
2018-06-25 23:12:18 +0300

    loading the items

--- app/src/main/java/com/job/jounalapp/ui/HomeFragment.java
+++ app/src/main/java/com/job/jounalapp/ui/HomeFragment.java
@@ -3,23 +3,35 @@ package com.job.jounalapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.job.jounalapp.R;
+import com.job.jounalapp.datasource.Dairy;
+import com.job.jounalapp.util.DairyViewHolder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+
+import static com.job.jounalapp.util.Constants.DAIRYCOL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +39,7 @@ import butterknife.Unbinder;
 public class HomeFragment extends Fragment {
 
 
+    private static final String TAG = "HomeFrag";
     @BindView(R.id.home_list)
     RecyclerView homeList;
     @BindView(R.id.home_fab)
@@ -34,6 +47,9 @@ public class HomeFragment extends Fragment {
     Unbinder unbinder;
 
     private FirestoreRecyclerAdapter adapter;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser mCurrentUser;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -56,11 +72,28 @@ public class HomeFragment extends Fragment {
         unbinder.unbind();
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+        setUpDairyList();
+    }
+
     @OnClick(R.id.home_fab)
     public void onViewClicked() {
         Intent intent = new Intent(getActivity(),AddItemActivity.class);
         startActivity(intent);
     }
+
+    /*
+    * This sets up a firestore recycler view that manages the list data as added int the database
+    * changes are real time and responsive
+    * */
 
     private void setUpDairyList(){
 
@@ -70,10 +103,42 @@ public class HomeFragment extends Fragment {
         homeList.setLayoutManager(linearLayoutManager);
 
         // Create a reference to the members collection
-        final CollectionReference transRef = mFirestore.collection(USERSTRANSACTIONCOL);
-        final Query query = transRef
+        final CollectionReference dairyRef = mFirestore.collection(DAIRYCOL);
+        final Query query = dairyRef
                 .whereEqualTo("userid",mCurrentUser.getUid())
                 .orderBy("timestamp", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<Dairy> options = new FirestoreRecyclerOptions.Builder<Dairy>()
+                .setQuery(query, Dairy.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<Dairy,DairyViewHolder>(options) {
+            @NonNull
+            @Override
+            public DairyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                // Create a new instance of the ViewHolder, in this case we are using a custom
+                // layout called R.layout.message for each item
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.single_dairy_item, parent, false);
+
+                return new DairyViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull DairyViewHolder holder, int position, @NonNull Dairy model) {
+
+            }
+
+            @Override
+            public void onError(@NonNull FirebaseFirestoreException e) {
+                super.onError(e);
+                Log.e(TAG, "onError: ", e);
+            }
+        };
+
+        adapter.startListening();
+        adapter.notifyDataSetChanged();
+        homeList.setAdapter(adapter);
     }
     @Override
     public void onStart() {
