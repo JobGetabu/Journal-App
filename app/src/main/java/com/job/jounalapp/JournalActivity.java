3984aac31b0ba62fd3eb5562f7ebd3ee88b6866b
JobGetabu <getabujob@gmail.com>
2018-06-25 14:37:22 +0300

    Added firebase to project and all needed dependencies

---
+++ app/src/main/java/com/job/jounalapp/JournalActivity.java
@@ -0,0 +1,13 @@
+package com.job.jounalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+    }
+}
