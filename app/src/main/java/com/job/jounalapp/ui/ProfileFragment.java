4bf675b6aed71f07f7ff3c4cbd5bc830c2fb5d5f
JobGetabu <getabujob@gmail.com>
2018-06-25 16:33:06 +0300

    added firebase core libs

---
+++ app/src/main/java/com/job/jounalapp/ui/ProfileFragment.java
@@ -0,0 +1,32 @@
+package com.job.jounalapp.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.job.jounalapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+
+}
