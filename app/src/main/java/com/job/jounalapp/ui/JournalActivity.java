7e5763064f09b6170eaa27708e8fef8baf714924
JobGetabu <getabujob@gmail.com>
2018-06-25 14:38:49 +0300

    structured the architecture: packaging

--- app/src/main/java/com/job/jounalapp/ui/JournalActivity.java
+++ app/src/main/java/com/job/jounalapp/ui/JournalActivity.java
@@ -1,7 +1,9 @@
-package com.job.jounalapp;
+package com.job.jounalapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.job.jounalapp.R;
 
 public class JournalActivity extends AppCompatActivity {
 
